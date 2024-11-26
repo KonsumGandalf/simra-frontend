b32c6b5a4ee636a55df8be0a56d460fbcb965814
KonumGandalf <david_schmidt_privat@outlook.com>
2024-11-26 23:14:59 +0100

    feat(app): not-found
    Add a not found page to catch false requests.

---
+++ src/app/pages/not-found/not-found.page.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+	selector: 'app-not-found',
+	standalone: true,
+	imports: [ CommonModule, RouterLink ],
+	templateUrl: './not-found.page.html',
+	styleUrl: './not-found.page.scss',
+	encapsulation: ViewEncapsulation.None,
+	changeDetection: ChangeDetectionStrategy.OnPush,
+	host: {
+		class: 'app-not-found',
+	},
+})
+export class NotFoundPage {}
