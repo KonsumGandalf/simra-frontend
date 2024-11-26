c3436496db01bea2f30c1e2c425b722e9639d5e4
KonumGandalf <david_schmidt_privat@outlook.com>
2024-11-26 23:16:33 +0100

    feat(rides-incident-shell): initialize lib

---
+++ libs/rides/incident/shell/README.md
@@ -0,0 +1,9 @@
+# rides-incident-shell
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test rides-incident-shell` to execute the unit tests.
+
+Run to generate `nx g @nx/angular:library libs/rides/incident/shell --tags=shell --name=rides-incident-shell`
---
+++ libs/rides/incident/shell/src/lib/shell.routes.ts
@@ -0,0 +1,20 @@
+import { Route } from '@angular/router';
+
+export const RIDE_incident_SHELL_ROUTES: Route[] = [
+  {
+    path: '',
+    children: [
+      {
+        path: 'exploring-map',
+        loadComponent: () =>
+          import('@simra/rides-incident-map').then(
+            (m) => m.ExploringMapComponent
+          ),
+      },
+      {
+        path: '**',
+        redirectTo: 'exploring-map',
+      },
+    ],
+  },
+];
