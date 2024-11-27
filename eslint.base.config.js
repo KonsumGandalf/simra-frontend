b08848c84f74e56913fb317600ea66bb70eed12a
KonumGandalf <david_schmidt_privat@outlook.com>
2024-11-27 01:13:38 +0100

    chore: move base eslint config

--- eslint.base.config.js
+++ eslint.base.config.js
@@ -20,6 +20,92 @@ module.exports = [
 							sourceTag: '*',
 							onlyDependOnLibsWithTags: ['*'],
 						},
+						{
+							sourceTag: 'type:master-feature',
+							onlyDependOnLibsWithTags: [
+								'type:feature',
+								'type:util-feature',
+								'type:util',
+								'type:common-model',
+							],
+						},
+						{
+							sourceTag: 'type:feature',
+							onlyDependOnLibsWithTags: [
+								'type:feature',
+								'type:util-feature',
+								'type:util',
+								'type:common-model',
+								'type:ui',
+							],
+						},
+						{
+							sourceTag: 'type:util-feature',
+							onlyDependOnLibsWithTags: [
+								'type:util',
+								'type:util-feature',
+								'type:common-model',
+							],
+						},
+						{
+							sourceTag: 'type:util',
+							onlyDependOnLibsWithTags: ['type:util', 'type:common-model'],
+						},
+						{
+							sourceTag: 'type:common-model',
+							onlyDependOnLibsWithTags: ['type:common-model'],
+						},
+						{
+							sourceTag: 'type:ui',
+							onlyDependOnLibsWithTags: [
+								'type:ui',
+								'type:atoms-ui',
+								'type:molecules-ui',
+								'type:organisms-ui',
+							],
+						},
+						{
+							sourceTag: 'type:atoms-ui',
+							onlyDependOnLibsWithTags: ['type:ui'],
+						},
+						{
+							sourceTag: 'type:molecules-ui',
+							onlyDependOnLibsWithTags: [
+								'type:ui',
+								'type:atoms-ui',
+								'type:molecules-ui',
+							],
+						},
+						{
+							sourceTag: 'type:organisms-ui',
+							onlyDependOnLibsWithTags: [
+								'type:ui',
+								'type:atoms-ui',
+								'type:molecules-ui',
+								'type:organisms-ui',
+							],
+						},
+						{
+							sourceTag: 'type:page-ui',
+							onlyDependOnLibsWithTags: [
+								'type:ui',
+								'type:atoms-ui',
+								'type:molecules-ui',
+								'type:organisms-ui',
+							],
+						},
+						{
+							sourceTag: 'domain:shared',
+							onlyDependOnLibsWithTags: ['domain:shared'],
+						},
+						{
+							sourceTag: 'domain:rides',
+							onlyDependOnLibsWithTags: ['domain:shared', 'domain:rides'],
+						},
+						{
+							sourceTag: 'domain:incident',
+							onlyDependOnLibsWithTags: ['domain:shared', 'domain:incident'],
+						}
 					],
 				},
 			],
@@ -29,5 +115,24 @@ module.exports = [
 		files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
 		// Override or add rules here
 		rules: {},
+	},
+	{
+		files: ['*.scss'],
+		extends: ['stylelint'],
+		rules: {},
+	},
+	{
+		files: ['*.ts', '*.tsx'],
+		extends: [
+			'plugin:@nx/typescript',
+			'plugin:@typescript-eslint/eslint-recommended',
+			'plugin:@typescript-eslint/recommended',
+		],
+		plugins: ['simple-import-sort', 'prettier'],
+		rules: {
+			'simple-import-sort/imports': 'error',
+			'@typescript-eslint/no-explicit-any': 'off',
+			'prettier/prettier': 'error',
+		},
 	},
 ];
