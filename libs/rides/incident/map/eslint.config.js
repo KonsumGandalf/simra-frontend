7b048eb9e8c794c905b86f7a75c5c1e2fdd6bb5b
KonumGandalf <david_schmidt_privat@outlook.com>
2024-12-17 15:21:32 +0100

    feat(rides-incident-map): incident-exploring-map
    Use map component and new api.

--- libs/rides/incident/map/eslint.config.js
+++ libs/rides/incident/map/eslint.config.js
@@ -1,34 +1,19 @@
 const nx = require('@nx/eslint-plugin');
-const baseConfig = require('../../../../eslint.base.config.js');
+const baseConfig = require('../../../../eslint.config.js');
 
 module.exports = [
 	...baseConfig,
 	...nx.configs['flat/angular'],
 	...nx.configs['flat/angular-template'],
 	{
-		files: ['**/*.ts'],
-		rules: {
-			'@angular-eslint/directive-selector': [
-				'error',
-				{
-					type: 'attribute',
-					prefix: 'simra',
-					style: 'camelCase',
-				},
-			],
-			'@angular-eslint/component-selector': [
-				'error',
-				{
-					type: 'element',
-					prefix: 'simra',
-					style: 'kebab-case',
-				},
-			],
-		},
-	},
-	{
 		files: ['**/*.html'],
 		// Override or add rules here
 		rules: {},
+	},
+	{
+		files: ['**/*.ts'],
+		rules: {
+			'@angular-eslint/prefer-standalone': 'off',
+		},
 	},
 ];
