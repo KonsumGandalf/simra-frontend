36f4789d7e4795937caeb931f1d0fb64e34518ab
David Schmidt <66205241+KonsumGandalf@users.noreply.github.com>
2025-01-08 20:14:32 +0100

    feat: storybook
    Add storybook with compodocs for the

---
+++ .storybook/main.ts
@@ -0,0 +1,52 @@
+import type { StorybookConfig } from '@storybook/angular';
+
+const config: StorybookConfig = {
+	stories: [
+		'../src/app/**/*.@(mdx|stories.@(js|jsx|ts|tsx))',
+		{
+			directory: '../libs/common/ui/components/src/lib/atoms',
+			titlePrefix: 'Atoms/'
+		},
+		{
+			directory: '../libs/common/ui/components/src/lib/molecules',
+			titlePrefix: 'Molecules/'
+		},
+		{
+			directory: '../libs/common/ui/components/src/lib/templates',
+			titlePrefix: 'Templates/'
+		},
+	],
+	staticDirs: [
+		{
+			from: '../libs/common/ui/components/assets/i18n',
+			to: '/assets/i18n/common/ui/components'
+		}
+	],
+	addons: [
+        '@storybook/addon-essentials',
+        '@storybook/addon-interactions',
+        '@storybook/addon-styling-webpack'
+    ],
+	framework: {
+		name: '@storybook/angular',
+		options: {},
+	},
+	core: {
+		builder: {
+			name: 'webpack5',
+			options: {
+				lazyCompilation: true,
+				fsCache: true,
+			}
+		}
+	},
+	docs: {
+		defaultName: 'Docs',
+	}
+};
+
+export default config;
+
+// To customize your webpack configuration you can use the webpackFinal field.
+// Check https://storybook.js.org/docs/react/builders/webpack#extending-storybooks-webpack-config
+// and https://nx.dev/recipes/storybook/custom-builder-configs
---
+++ .storybook/preview.ts
@@ -0,0 +1,49 @@
+import { APP_INITIALIZER } from '@angular/core';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { applicationConfig, Preview } from '@storybook/angular';
+import Aura from '@primeng/themes/aura';
+import { PrimeNG } from 'primeng/config';
+import { setCompodocJson } from '@storybook/addon-docs/angular';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import docJson from './documentation.json';
+setCompodocJson(docJson);
+
+function provideTheme(config: PrimeNG) {
+	return () => {
+		config.theme.set({
+			preset: Aura,
+		});
+	};
+}
+
+const preview: Preview = {
+	decorators: [
+		applicationConfig({
+			providers: [
+				provideAnimations(),
+				{
+					provide: APP_INITIALIZER,
+					useFactory: provideTheme,
+					deps: [PrimeNG ],
+					multi: true,
+				},
+			],
+		}),
+	],
+	tags: ['autodocs'],
+	parameters: {
+		docs: {
+			story: {
+				inline: true
+			}
+		},
+		controls: {
+			expanded: true
+		}
+	}
+};
+
+
+
+export default preview;
