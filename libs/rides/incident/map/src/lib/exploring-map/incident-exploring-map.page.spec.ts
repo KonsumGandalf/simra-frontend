4de068e466325605e21c021b1818e1b3bbafac27
David Schmidt <66205241+KonsumGandalf@users.noreply.github.com>
2025-01-08 20:46:55 +0100

    chore(rides): eslint and tests

--- libs/rides/incident/map/src/lib/exploring-map/incident-exploring-map.page.spec.ts
+++ libs/rides/incident/map/src/lib/exploring-map/incident-exploring-map.page.spec.ts
@@ -1,16 +1,21 @@
 import 'reflect-metadata';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TranslateModule } from '@ngx-translate/core';
 import { ExploringMapFacade } from '@simra/rides-incident-domain';
 import { of } from 'rxjs';
 import { IncidentExploringMapPage } from './incident-exploring-map.page';
 
-describe('ExploringMapComponent', () => {
+describe('ExploringMapPage', () => {
 	let component: IncidentExploringMapPage;
 	let fixture: ComponentFixture<IncidentExploringMapPage>;
 
 	beforeEach(async () => {
 		await TestBed.configureTestingModule({
-			imports: [IncidentExploringMapPage],
+			imports: [
+				IncidentExploringMapPage,
+
+				TranslateModule.forRoot()
+			],
 			providers: [
 				{
 					provide: ExploringMapFacade,
