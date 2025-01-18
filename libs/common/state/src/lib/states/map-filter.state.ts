4dec9149348352a196ceac70d0a9c45793699e4e
David Schmidt <66205241+KonsumGandalf@users.noreply.github.com>
2025-01-18 16:05:51 +0100

    feat(common-state): Ngxs integration
    Add ngxs to manage states. Track which street was clicked last.

---
+++ libs/common/state/src/lib/states/map-filter.state.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Action, Selector, State, StateContext } from '@ngxs/store';
+import { MapFilterOptionsInterface } from '@simra/common-models';
+import { UpdateFilterOptions } from '../actions/map-filter.actions';
+
+@State<MapFilterOptionsInterface>({
+	name: 'map_filter',
+	defaults: {
+		weekDay: undefined,
+		trafficTime: undefined,
+	}
+})
+@Injectable()
+export class MapFilterState {
+	@Selector()
+	static getMapFilterState(state: MapFilterOptionsInterface) {
+		return state;
+	}
+
+	@Action(UpdateFilterOptions)
+	addToStreetCache(ctx: StateContext<MapFilterOptionsInterface>, action: UpdateFilterOptions) {
+		ctx.patchState({
+			...action.filterOptions
+		});
+	}
+}
