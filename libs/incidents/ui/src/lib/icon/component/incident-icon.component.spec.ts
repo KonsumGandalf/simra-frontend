ed9e1b3f1d621de6793f3ea155c091d720212bd8
David Schmidt <66205241+KonsumGandalf@users.noreply.github.com>
2025-02-02 22:21:13 +0100

    test(incidents): fix test

--- libs/incidents/ui/src/lib/icon/component/incident-icon.component.spec.ts
+++ libs/incidents/ui/src/lib/icon/component/incident-icon.component.spec.ts
@@ -5,14 +5,18 @@ import { IncidentIconComponent } from './incident-icon.component';
 describe('IconComponent', () => {
 	let component: IncidentIconComponent;
 	let fixture: ComponentFixture<IncidentIconComponent>;
+	let mockToolTips: string[];
 
 	beforeEach(async () => {
+		mockToolTips = ['test1', 'test2', 'test3'];
+
 		await TestBed.configureTestingModule({
 			imports: [IncidentIconComponent, TranslateModule.forRoot()],
 		}).compileComponents();
 
 		fixture = TestBed.createComponent(IncidentIconComponent);
 		component = fixture.componentInstance;
+		component.tooltips = mockToolTips
 		fixture.detectChanges();
 	});
 
