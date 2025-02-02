0d492d4e76e34f968a8eed7371fa4ae968d19140
David Schmidt <66205241+KonsumGandalf@users.noreply.github.com>
2025-02-03 00:27:03 +0100

    feat(incidents-ui): custom marker

--- libs/incidents/ui/src/lib/functions/create-custom-map-pin.ts
+++ libs/incidents/ui/src/lib/functions/create-custom-map-pin.ts
@@ -1,11 +1,12 @@
 import { divIcon, DivIcon } from 'leaflet';
 
 export function createCustomMapPin(isScary: boolean): DivIcon {
-	const color = isScary ? 'text-red-500' : 'text-blue-500'; // Dynamic color
+	const svgUrl = `assets/icons/incidents/ui/pin-${isScary ? 'red': 'blue'}.svg`;
 
-	const className = `ph-fill ph-map-pin-simple text-4xl bg-transparent ${color}`; // Dynamic color
+	const className = `text-4xl`;
 	return divIcon({
 		className: className,
+		html: `<img src="${svgUrl}" class="w-8 h-8" />`,
 		iconSize: [32, 32],
 		iconAnchor: [16, 32],
 		popupAnchor: [0, -32]
