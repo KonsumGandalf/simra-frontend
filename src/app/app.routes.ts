07893833a504027db084e3cd8fb52898eb371ecd
David Schmidt <66205241+KonsumGandalf@users.noreply.github.com>
2025-02-02 21:23:27 +0100

    feat: rides route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,8 @@ import { Route } from '@angular/router';
 
 export const APP_ROUTES: Route[] = [
 	{
-		path: 'rides/incidents',
-		loadChildren: () =>
-			import('@simra/rides-incident-shell').then((m) => m.RIDE_INCIDENT_SHELL_ROUTES),
+		path: 'rides',
+		loadChildren: () => import('@simra/rides-shell').then((m) => m.RIDES_SHELL_ROUTES),
 	},
 	{
 		path: 'streets',
