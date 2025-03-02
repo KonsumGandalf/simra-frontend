b62c548b51d61c5f8d7c9dcda35fced5b8201366
David Schmidt <66205241+KonsumGandalf@users.noreply.github.com>
2025-03-02 15:53:27 +0100

    refactor: i18n translations

--- libs/common/ui/components/src/lib/translations/maps/public-api.ts
+++ libs/common/ui/components/src/lib/translations/maps/public-api.ts
@@ -1,5 +1,2 @@
-export * from './cycleway-lanes-to-translation.map';
-export * from './highway-types-to-translation';
-export * from './street-parking-to-translation.map';
 export * from './traffic-times-to-translation';
 export * from './week-days-to-translation';
