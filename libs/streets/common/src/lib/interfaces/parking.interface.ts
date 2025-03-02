32aaab475d3b519ceb8ca5e5bc00f5571e2c3162
David Schmidt <66205241+KonsumGandalf@users.noreply.github.com>
2025-03-02 13:26:23 +0100

    feat(streets-common): interfaces
    Add tag interfaces to allow displaying details of streets

---
+++ libs/streets/common/src/lib/interfaces/parking.interface.ts
@@ -0,0 +1,8 @@
+import { EStreetParking } from '@simra/common-models';
+
+/**
+ * Indicates how the parking situation is
+ */
+export interface IParking {
+	type: EStreetParking;
+}
--- libs/streets/common/src/lib/interfaces/response-street.interface.ts
+++ libs/streets/common/src/lib/interfaces/response-street.interface.ts
@@ -1,13 +1,13 @@
-import { IncidentInterface } from '@simra/incidents-models';
+import { IIncident } from '@simra/incidents-models';
 import { ISafetyMetrics } from '../interfaces/safety-metrics.interface';
 import { ITags } from './tags.interface';
 
 export interface IResponseStreet {
 	id: number;
 	safetyMetrics: ISafetyMetrics[];
-	rideIncident: IncidentInterface[];
+	rideIncident: IIncident[];
 	way: any;
 	highway: string;
 	name: string;
-	tags: ITags
+	tags: ITags;
 }
