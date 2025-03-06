2f310b4a3af6235329ba88e949c101c351de86d9
David Schmidt <66205241+KonsumGandalf@users.noreply.github.com>
2025-03-06 22:01:47 +0100

    test(app): footer
    fix test

--- src/app/components/footer/component/footer.component.spec.ts
+++ src/app/components/footer/component/footer.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TranslateModule } from '@ngx-translate/core';
 import { FooterComponent } from './footer.component';
 
 describe('FooterComponent', () => {
@@ -7,7 +8,7 @@ describe('FooterComponent', () => {
 
 	beforeEach(async () => {
 		await TestBed.configureTestingModule({
-			imports: [FooterComponent],
+			imports: [FooterComponent, TranslateModule.forRoot()],
 		}).compileComponents();
 
 		fixture = TestBed.createComponent(FooterComponent);
